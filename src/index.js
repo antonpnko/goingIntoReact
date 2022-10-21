506547e275e2e5e7383c6728aa4ed3fffd936cbc
antonpnko <antonpnko@users.noreply.github.com>
2022-10-21 13:19:11 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client';
+
+import RoutesMain from 'routesMain'
+import  NavBar from 'components/navBar'
+import { BrowserRouter as Router } from "react-router-dom";
+
+const App = () => {
+    return (
+        <div>
+            <Router>
+                <NavBar />
+                <RoutesMain />
+            </Router>
+            hello
+        </div>
+    )
+}
+
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
+
